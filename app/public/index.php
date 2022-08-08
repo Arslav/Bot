62df6188cb9ef475565d5f633d3de579ee228b1b
arslav <ardesslava@gmail.com>
2022-08-08 23:02:52 +0500

    Изменил тип проекта на библиотеку

--- app/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Arslav\Newbot\App;
-
-$container = require __DIR__ . '/../bootstrap.php';
-
-$app = new App($container);
-$app->run();
--- app/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Arslav\Newbot\App;
-
-$_ENV['DB_NAME']='test';
-
-$container = require_once __DIR__ . '/../bootstrap.php';
-
-return new App($container);
